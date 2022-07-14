756ca5acbfa61cc11604f97ea2bd2f55433c446d
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-07-14 12:08:54 +0900

    TODO App - 프로젝트 시작

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- v-model은 two-way binding -->
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      newTodoItem: '',
+    };
+  },
+  methods: {
+    addTodo: function () {
+      // console.log(this.newTodoItem);
+      // 저장하는 로직 후, input 내용을 지우면 된다.
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = '';
+    },
+  },
+};
+</script>
+
+<style></style>
